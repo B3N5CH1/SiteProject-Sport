f13a3a934be9461a5ad0f0c42a34bce48a545032
Benjamin Graf <benjamin.graf@gmx.ch>
2014-09-25 11:21:55 +0200

    index.php & submit.php navbar adjust

---
+++ index.php
@@ -0,0 +1,68 @@
+<html>
+	<head> 
+        <title>Broadsport - Sportveranstaltungen in der Nähe</title>
+        <meta charset="utf-8">
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.css" >
+		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+	</head>
+	<body>
+        <div class="container">
+            <p></p>
+            <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+                <div class="container">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collabse-1">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="index.php">Home</a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav">
+                                <li><a href="submit.php">Veranstaltung eintragen</a></li>
+                                <li><a href="About.html">About</a></li>
+                                <!--<li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="#">Action</a></li>
+                                        <li><a href="#">Another action</a></li>
+                                        <li><a href="#">Something else here</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Separated link</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">One more separated link</a></li>
+                                    </ul> -->
+                                </li>
+                            </ul>
+                            <form class="navbar-form navbar-left" role="search">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Search">
+                                </div>
+                                <button type="submit" class="btn btn-default">Submit</button>
+                            </form> <!--
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="#">Link</a></li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="#">Action</a></li>
+                                        <li><a href="#">Another action</a></li>
+                                        <li><a href="#">Something else here</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Separated link</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div> -->
+                    </div>
+                </div>
+            </nav>
+        
+        <h1>Broadsport - Sportveranstaltungen in der Nähe</h1>
+        <h2>News / Updates</h2>
+        </div>
+    </body>
+</html>
--- submit.php
+++ submit.php
@@ -23,11 +23,11 @@
                                 <span class="icon-bar"></span>
                                 <span class="icon-bar"></span>
                             </button>
-                            <a class="navbar-brand" href="index.html">Home</a>
+                            <a class="navbar-brand" href="index.php">Home</a>
                         </div>
                         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                             <ul class="nav navbar-nav">
-                                <li><a href="submit.html">Veranstaltung eintragen</a></li>
+                                <li><a href="submit.php">Veranstaltung eintragen</a></li>
                                 <li><a href="About.html">About</a></li>
                                 <!--<li class="dropdown">
                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
